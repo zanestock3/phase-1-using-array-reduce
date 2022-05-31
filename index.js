48ae847ea67496e56f1eb21e072d3f38b33dff57
Zane Stock <zanestock3@gmail.com>
2022-05-31 07:25:43 -0500

    commit tree b24fe933246bd3ee2cacd40fcf50daf65c976915

--- index.js
+++ index.js
@@ -1,3 +1,9 @@
 const batteryBatches = [4, 5, 3, 4, 4, 6, 5];
 
 // Code your solution here
+const sumWithTotal = 0;
+const totalBatteries = batteryBatches.reduce(
+    (previousValue, currentValue) => previousValue + currentValue,
+    sumWithTotal
+  );
+totalBatteries;
